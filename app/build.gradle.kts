4b8a4861a0b457a5b36a869f9d959c3794b33d28
ryujeeKim <130418731+ryujeeKim@users.noreply.github.com>
2025-01-07 15:51:35 +0900

    마이홈 b-d일단 레이아웃만,,,,,,,,,,

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.umc.upstyle"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.umc.upstyle"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 

237be6830bcff1c5c1ea6f84fa199e0a1a08da09
yuuuhooo <yuuuhooo13@gmail.com>
2025-01-14 04:11:43 +0900

    Feat: ClosetResultFragment 구현

--- app/src/main/java/com/umc/upstyle/ClosetResultFragment.kt
+++ app/src/main/java/com/umc/upstyle/ClosetResultFragment.kt
@@ -5,55 +5,35 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.umc.upstyle.databinding.FragmentClosetResultBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [ClosetResultFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class ClosetResultFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private var _binding: FragmentClosetResultBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_closet_result, container, false)
+        _binding = FragmentClosetResultBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ClosetResultFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ClosetResultFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // arguments에서 전달된 데이터 받기
+        val where = arguments?.getString("where") ?: "기본값1"
+        val param2 = arguments?.getString("param2") ?: "기본값2"
+        val param3 = arguments?.getString("param3") ?: "기본값3"
+
+        // TextView에 데이터 설정
+        binding.mainTitleTextView.text = where
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }

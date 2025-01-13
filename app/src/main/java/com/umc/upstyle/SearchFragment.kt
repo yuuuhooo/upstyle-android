b54406b3ec0941a34e3ff1d198bb571ecff1b907
yuuuhooo <yuuuhooo13@gmail.com>
2025-01-14 00:38:53 +0900

    Feat: SearchFragment 화면 구현

--- app/src/main/java/com/umc/upstyle/SearchFragment.kt
+++ app/src/main/java/com/umc/upstyle/SearchFragment.kt
@@ -5,7 +5,34 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.umc.upstyle.databinding.FragmentSearchBinding
 
-class SearchFragment : Fragment(R.layout.activity_outer_1) {
-    // 필요한 UI 요소와 동작을 여기에 추가할 수 있습니다
+// 필요한 UI 요소와 동작을 여기에 추가할 수 있습니다
+class SearchFragment : Fragment() {
+
+    private var _binding: FragmentSearchBinding? = null
+    private val binding get() = _binding!! // Non-nullable로 사용하기 위한 접근자
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View? {
+        // 뷰 바인딩 초기화
+        _binding = FragmentSearchBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // UI 작업 및 이벤트 처리
+//        binding.myButton.setOnClickListener {
+//            // 버튼 클릭 이벤트 처리
+//        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        // 메모리 누수 방지: 뷰가 파괴되면 바인딩 해제
+        _binding = null
+    }
 }

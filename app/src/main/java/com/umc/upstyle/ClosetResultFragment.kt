e80607cb1e017f5e4ad9f9e2051345a512a5285b
yuuuhooo <yuuuhooo13@gmail.com>
2025-01-14 19:13:40 +0900

    Fix: TextView 데이터 로드 방식으로 수정

--- app/src/main/java/com/umc/upstyle/ClosetResultFragment.kt
+++ app/src/main/java/com/umc/upstyle/ClosetResultFragment.kt
@@ -6,6 +6,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.umc.upstyle.databinding.FragmentClosetResultBinding
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import java.io.File
 
 class ClosetResultFragment : Fragment() {
 
@@ -28,8 +32,18 @@ class ClosetResultFragment : Fragment() {
         val param2 = arguments?.getString("param2") ?: "기본값2"
         val param3 = arguments?.getString("param3") ?: "기본값3"
 
-        // TextView에 데이터 설정
+        // 어디서 온 건지 TextView에 데이터 설정 ex. OUTER, TOP
         binding.mainTitleTextView.text = where
+
+        // arguments에서 전달된 리스트 받기
+        val colorList = arguments?.getStringArrayList("colorList") ?: arrayListOf()
+
+        // 리스트를 ", "로 구분하여 하나의 문자열로 결합
+        val resultText = colorList.joinToString(", ")
+
+        // TextView에 선택된 컬러들 보여주기
+        binding.tvSelectedOptions.text = resultText
+
     }
 
     override fun onDestroyView() {

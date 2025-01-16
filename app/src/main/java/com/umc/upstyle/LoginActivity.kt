e3d22e708caf908a1ce306393ce5b9f2b46b9e0c
yuuuhooo <yuuuhooo13@gmail.com>
2025-01-16 17:19:45 +0900

    Style: 불필요한 패키지 삭제

--- app/src/main/java/com/umc/upstyle/LoginActivity.kt
+++ app/src/main/java/com/umc/upstyle/LoginActivity.kt
@@ -4,9 +4,6 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import com.umc.upstyle.databinding.ActivityBodyinfoBinding
 import com.umc.upstyle.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
@@ -14,6 +11,7 @@ class LoginActivity : AppCompatActivity() {
     private lateinit var binding: ActivityLoginBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         // ViewBinding 초기화

9ba7f0be6b8206d36f3454cec6fbec1289e56f0f
yuuuhooo <yuuuhooo13@gmail.com>
2025-01-26 18:04:42 +0900

    Docs: 주석 추가

--- app/src/main/java/com/umc/upstyle/LoginActivity.kt
+++ app/src/main/java/com/umc/upstyle/LoginActivity.kt
@@ -20,12 +20,16 @@ class LoginActivity : AppCompatActivity() {
         // setContentView()로 ViewBinding의 root 레이아웃을 설정
         setContentView(binding.root)
 
-
+        // 비회원으로 둘러보기
         binding.tvNonMember.setOnClickListener {
             // 버튼 클릭 이벤트 처리
             // Intent 생성
             val intent = Intent(this, BodyinfoActivity::class.java)
             startActivity(intent) // 현재는 TestActivity로 이동. 실제로는 마이홈으로 가야 됨.
         }
+
+
+        // 구글로그인 구현부
+
     }
 }

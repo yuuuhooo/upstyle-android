ccac49e0ed04f70f9c639086d6d0c230be435526
yuuuhooo <yuuuhooo13@gmail.com>
2025-02-02 17:51:41 +0900

    Feat: 커뮤니티 화면 기능 구현

--- app/src/main/java/com/umc/upstyle/CreateVoteFragment.kt
+++ app/src/main/java/com/umc/upstyle/CreateVoteFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.umc.upstyle.databinding.ActivityCategoryBinding
 import com.umc.upstyle.databinding.FragmentCreateVoteBinding
 
 
@@ -21,5 +22,13 @@ class CreateVoteFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        val goBackButton = binding.backButton
+        goBackButton.setOnClickListener {
+            requireActivity().supportFragmentManager.popBackStack()  // 이전 화면으로 돌아가기
+        }
     }
 }
+
+
+

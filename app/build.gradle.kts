08e7cee76798adb9037e886c7211432a6cd68113
yuuuhooo <yuuuhooo13@gmail.com>
2025-02-04 22:37:48 +0900

    레트로핏 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -113,5 +113,9 @@ dependencies {
 
     // Viewmodel
     implementation ("androidx.lifecycle:lifecycle-viewmodel-ktx:2.5.1")
+
+    implementation("com.squareup.retrofit2:retrofit:2.11.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.11.0")
+    implementation("com.squareup.retrofit2:adapter-rxjava2:2.11.0")
 }
 

10c9f1cb44b3e6d55e082f7df716e24f6070b9cd
yuuuhooo <yuuuhooo13@gmail.com>
2025-02-16 05:20:05 +0900

    Feat: 투표 기능 구현

--- app/src/main/java/com/umc/upstyle/data/network/VoteService.kt
+++ app/src/main/java/com/umc/upstyle/data/network/VoteService.kt
@@ -2,11 +2,23 @@ package com.umc.upstyle.data.network
 
 import com.umc.upstyle.data.model.ApiResponse
 import com.umc.upstyle.data.model.VoteDetailResponse
+import com.umc.upstyle.data.model.VoteResponseRequest
+import com.umc.upstyle.data.model.VoteResult
 import retrofit2.Response
+import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.POST
 import retrofit2.http.Path
 
 interface VoteService {
     @GET("votes/{voteId}")
-    suspend fun getVoteDetail(@Path("voteId") voteId: Int): Response<ApiResponse<VoteDetailResponse>>
+    suspend fun getVoteDetail(
+        @Path("voteId") voteId: Int
+    ): Response<ApiResponse<VoteDetailResponse>>
+
+    @POST("/votes/{voteId}/response")
+    suspend fun submitVoteResponse(
+        @Path("voteId") voteId: Int,
+        @Body request: VoteResponseRequest
+    ): Response<ApiResponse<VoteResult>>
 }
